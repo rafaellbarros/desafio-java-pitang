9b9582dc9fad1110af71532700cc98478a00daa6
Rafael Barros <rafaelbarros.softwareengineer@gmail.com>
2024-03-30 00:16:07 -0300

    :construction: Implementando Service Gateway

---
+++ car-user-system/gateway/src/test/java/br/com/rafaellbarros/gateway/GatewayAppTests.java
@@ -0,0 +1,11 @@
+package br.com.rafaellbarros.gateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GatewayAppTests {
+    @Test
+    public void contextLoads() {
+    }
+}
