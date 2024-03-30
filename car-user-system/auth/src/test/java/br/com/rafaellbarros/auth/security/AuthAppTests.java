72b32bc255aaefe35a36c82c1432858cc2930723
Rafael Barros <rafaelbarros.softwareengineer@gmail.com>
2024-03-30 00:19:41 -0300

    :construction: Implementando modulo security auth

---
+++ car-user-system/auth/src/test/java/br/com/rafaellbarros/auth/security/AuthAppTests.java
@@ -0,0 +1,14 @@
+package br.com.rafaellbarros.auth.security;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AuthAppTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
