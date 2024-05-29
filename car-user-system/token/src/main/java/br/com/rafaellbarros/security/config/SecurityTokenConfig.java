e9365ee3074cdf7b7831dd929a9206101d4f0cfb
Rafael Barros <rafaelbarros.softwareengineer@gmail.com>
2024-05-29 05:17:03 -0300

    :construction: Ajustes implementação token CORS e Docker

--- car-user-system/token/src/main/java/br/com/rafaellbarros/security/config/SecurityTokenConfig.java
+++ car-user-system/token/src/main/java/br/com/rafaellbarros/security/config/SecurityTokenConfig.java
@@ -2,10 +2,10 @@ package br.com.rafaellbarros.security.config;
 
 import br.com.rafaellbarros.core.property.JwtConfiguration;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.cors.CorsConfiguration;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,32 +13,30 @@ import javax.servlet.http.HttpServletResponse;
 @RequiredArgsConstructor
 public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
     private static final String[] AUTH_WHITE_LIST = {
-            "/**/token/**",
-            "/**/users/**",
-            "/**/swagger-ui.html",
-            "/**/swagger-resources/**",
-            "/**/webjars/springfox-swagger-ui/**",
-            "/**/v2/api-docs/**",
+            "/**/**/users/**",
+            "/**/**/swagger-ui.html",
+            "/**/**/swagger-resources/**",
+            "/**/**/webjars/springfox-swagger-ui/**",
+            "/**/**/v2/api-docs/**",
+
     };
 
-    private static final String [] AUTH_BLACK_LIST = {
-            "/back-end/api/cars/**"
+    private static final String[] AUTH_BLACK_LIST = {
+            "/**/**/**/cars/**"
     };
 
     protected final JwtConfiguration jwtConfiguration;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
-                .and()
+        http.cors().and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .exceptionHandling().authenticationEntryPoint((req, resp, e) -> resp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
                 .and()
                 .authorizeRequests()
                 .antMatchers(jwtConfiguration.getLoginUrl()).permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .antMatchers(AUTH_WHITE_LIST).permitAll()
                 .antMatchers(AUTH_BLACK_LIST).authenticated();
     }
